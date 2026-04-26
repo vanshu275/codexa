ebd3469f0a4458180e28187bf664c37bd6b05f3d
ChipperDoxy <114298045+ChipperDoxy@users.noreply.github.com>
2026-04-26 13:12:18 +0530

    made a project section

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,11 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
-// Yahan apna hero image import karle agar pehle se hai, warna line 17 check kar
-// import heroImage from "./assets/hero-bg.png"; 
+
 
 export default function App() {
   return (
     <main className="min-h-screen relative overflow-x-hidden bg-[#050505]">
-      {/* Global Background Decoration */}
-      <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full max-w-4xl pointer-events-none opacity-70 z-0">
-        {/* Agar heroImage nahi hai toh is <img> tag ko temporary comment kar dena */}
-        {/* <img src={heroImage} alt="" className="w-full h-auto" /> */}
-      </div>
 
       <div className="relative z-10">
         <Navbar />
